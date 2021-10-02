b5568c6fd1fa947a03a7aa3bc8ea6fe5b6e6a67f
Michael <edelsteinmichael23@gmail.com>
2021-10-02 12:48:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let greeting
+  if (distance <= 400) {
+      greeting = 'This one is on me!' 
+  } else if (distance > 2000) {
+      greeting = 'I will gladly take your thirty bucks.'
+  } if (distance >= 2500) {
+      greeting = 'No can do.'
+  }
+  return greeting  
+}
+  
+  
+
+function ternaryCheckCity(city){
+  const message = city = 'NYC' ? "Ok, sounds good." : "No go."
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+ 
 }
